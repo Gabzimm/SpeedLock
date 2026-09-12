26aac56715611f1352200835bc4b38c67990d8fd
Gabzimm <gabriele.s.leal.pro@gmail.com>
2026-09-12 13:55:06 +0100

    Delete lib directory

--- lib/state/access_request_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/access_request_service.dart';
-
-final accessRequestServiceProvider = Provider<AccessRequestService>((ref) => AccessRequestService());
